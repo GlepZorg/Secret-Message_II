e88696795a03212a3baf44d2a15ae7a7a98cc45d
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2024-02-25 17:04:43 -0800

    updated code

---
+++ app.js
@@ -0,0 +1,7 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+
+console.log(secretMessage.length);
+
+secretMessage.push('easily', 'right');
